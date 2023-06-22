a1d3366aa63a3611d64bce291eea3cb8a8ff693c
fajri.illahi <fajri.illahi@ifg-life.id>
2023-06-22 23:51:09 +0700

    @Setter and @Getter

--- src/main/java/com/orcaaa/entity/Customer.java
+++ src/main/java/com/orcaaa/entity/Customer.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "customers")
-@Data
-
+@Setter
+@Getter
 public class Customer {
     @Id
     @Column(name = "customer_id")
